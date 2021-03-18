dcd0dd7e7f3072f6a10deed92a23f018d5c7a1b2
Căldăraru Andrei Cătălin <catalin.caldararu99@gmail.com>
2021-03-18 10:26:30 +0200

    Singleton examples, eager,lazy,thread safe

---
+++ Seminar04/src/ro/ase/cts/clase/DepartamentFinanciar.java
@@ -0,0 +1,35 @@
+package ro.ase.cts.clase;
+
+public class DepartamentFinanciar {
+private int nrAngajati;
+private String director;
+private double salariulDeBaza;
+
+private static DepartamentFinanciar departamentFinanciar = null;
+
+private DepartamentFinanciar(int nrAngajati, String director, double salariulDeBaza){
+	this.director = director;
+	this.nrAngajati=nrAngajati;
+	this.salariulDeBaza=salariulDeBaza;
+}
+
+public static synchronized DepartamentFinanciar getInstance(int nrAngajati, String director, double salariuBaza) {
+	if(departamentFinanciar==null) {
+		departamentFinanciar=new DepartamentFinanciar(nrAngajati,director,salariuBaza);
+	}
+	return departamentFinanciar;
+}
+
+public void setNrAngajati(int nrAngajati) {
+	this.nrAngajati = nrAngajati;
+}
+
+public void setDirector(String director) {
+	this.director = director;
+}
+
+public void setSalariulDeBaza(double salariulDeBaza) {
+	this.salariulDeBaza = salariulDeBaza;
+}
+
+}
---
+++ Seminar04/src/ro/ase/cts/main/Main.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.main;
+import ro.ase.cts.clase.DepartamentFinanciar;
+import ro.ase.cts.clase.DepartamentFinanciarEager;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DepartamentFinanciarEager departamentFinanciarEager1= DepartamentFinanciarEager.getInstance();
+		DepartamentFinanciarEager departamentFinanciarEager2= DepartamentFinanciarEager.getInstance();
+		System.out.println(departamentFinanciarEager1.toString());
+		System.out.println(departamentFinanciarEager2.toString());
+		
+		departamentFinanciarEager1.setDirector("Gigel");
+		departamentFinanciarEager2.setNrAngajati(20);
+		System.out.println(departamentFinanciarEager1.toString());
+		System.out.println(departamentFinanciarEager2.toString());
+		
+		DepartamentFinanciar departamentFinanciar1= DepartamentFinanciar.getInstance(20,"Popescu",2500);
+		DepartamentFinanciar departamentFinanciar2= DepartamentFinanciar.getInstance(30,"Avramescu",2700);
+		
+		System.out.println(departamentFinanciar1.toString());
+		System.out.println(departamentFinanciar1.toString());
+	}
+}
