5b85980b5b6672e56bbbd8763af5fe734419577a
Căldăraru Andrei Cătălin <catalin.caldararu99@gmail.com>
2021-04-15 10:19:12 +0300

    Implemented the Composite exercise

---
+++ Seminar08/src/ro/ase/cts/composite/program/Main.java
@@ -0,0 +1,30 @@
+package ro.ase.cts.composite.program;
+
+import ro.ase.cts.composite.clase.Item;
+import ro.ase.cts.composite.clase.Sectiune;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Sectiune sectiuneBauturi = new Sectiune("Bauturi");
+	Sectiune sectiuneDesert = new Sectiune("Desert");
+	Item itemFrappe = new Item("Frappe");
+	Item itemCafea = new Item("Cafea");
+	Item itemAmandina = new Item("Amandina");
+	
+	sectiuneBauturi.addComponenta(itemCafea);
+	sectiuneBauturi.addComponenta(itemFrappe);
+	sectiuneDesert.addComponenta(itemAmandina);
+	
+	Sectiune meniu = new Sectiune("Meniu");
+	meniu.addComponenta(sectiuneDesert);
+	meniu.addComponenta(sectiuneBauturi);
+	
+	meniu.afiseazaDescriere();
+	sectiuneBauturi.stergeComponenta(itemFrappe);
+	sectiuneDesert.addComponenta(itemFrappe);
+	
+	meniu.afiseazaDescriere();
+	}
+
+}
