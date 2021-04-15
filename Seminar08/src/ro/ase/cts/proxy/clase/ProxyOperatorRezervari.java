2ab998a617fd3e7ebc8069d0f402e4b4c7260132
Căldăraru Andrei Cătălin <catalin.caldararu99@gmail.com>
2021-04-15 11:25:59 +0300

    Implemented the Proxy exercise

---
+++ Seminar08/src/ro/ase/cts/proxy/clase/ProxyOperatorRezervari.java
@@ -0,0 +1,22 @@
+package ro.ase.cts.proxy.clase;
+
+public class ProxyOperatorRezervari implements IOperatorRezervari{
+	private OperatorRezervari operator;
+	private int nrMinimPersoane;
+	
+	public ProxyOperatorRezervari(OperatorRezervari operator, int nrMinimPersoane) {
+		super();
+		this.operator = operator;
+		this.nrMinimPersoane = nrMinimPersoane;
+	}
+	
+	@Override
+	public void realizeazaRezervare(int nrPersoane)
+	{
+		if(nrPersoane >= nrMinimPersoane)
+		{
+			operator.realizeazaRezervare(nrPersoane);
+		}
+		else System.out.println("Va rugam veniti la restaurant fara rezervare");
+	}
+}
