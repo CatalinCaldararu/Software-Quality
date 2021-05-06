64bb10553722a680f12c40ce0fc6b95ea5c4a7c8
Căldăraru Andrei Cătălin <catalin.caldararu99@gmail.com>
2021-05-06 10:18:29 +0300

    Modified the main for the Command Exercise

--- Seminar10/src/ro/ase/cts/command/program/Main.java
+++ Seminar10/src/ro/ase/cts/command/program/Main.java
@@ -2,6 +2,7 @@ package ro.ase.cts.command.program;
 
 import ro.ase.cts.command.clase.ComandaConstituire;
 import ro.ase.cts.command.clase.ComandaDepunere;
+import ro.ase.cts.command.clase.ComandaRetragere;
 import ro.ase.cts.command.clase.ContBancar;
 import ro.ase.cts.command.clase.ManagerComenzi;
 
@@ -12,9 +13,11 @@ public class Main {
 		ContBancar contBancar = new ContBancar("Teo",0);
 		managerComenzi.invoca(new ComandaConstituire(contBancar,1000));
 		managerComenzi.invoca(new ComandaDepunere(contBancar,1000));
+		managerComenzi.invoca(new ComandaRetragere(contBancar,50));
 		
 		managerComenzi.executaComanda();
-
+		managerComenzi.executaComanda();
+		managerComenzi.executaComanda();
 	}
 
 }
